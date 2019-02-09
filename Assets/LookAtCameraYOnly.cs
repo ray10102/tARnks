17ebe5afb1281749dfe700cb4651ea09bd4b9c7f
ray <ray10102@gmail.com>
2019-02-09 17:34:07 -0500

    temp

---
+++ Assets/LookAtCameraYOnly.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class LookAtCameraYOnly : MonoBehaviour
+{
+    [SerializeField] private Camera cameraToLookAt;
+
+    void Start()
+    {
+        if (cameraToLookAt == null)
+        {
+            cameraToLookAt = Camera.main;
+        }
+    }
+    
+    void Update() 
+    {
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(cameraToLookAt.transform.position - v); 
+    }
+}
---
+++ Assets/SpriteHealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteHealthBar : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer left, right, top, bottom, back, front;
+    [SerializeField] private ParticleSystem brickChunks;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        
+    }
+}
