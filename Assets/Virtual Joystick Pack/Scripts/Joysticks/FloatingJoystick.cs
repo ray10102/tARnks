863b40fdf4db392692c2c38577ccb692ef8a961c
shanetimmerman@gmail.com <shane>
2019-02-09 16:15:55 -0500

    Moved files around

--- Assets/Virtual Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
+++ Assets/Virtual Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -8,25 +8,14 @@ public class FloatingJoystick : Joystick
     void Start()
     {
         background.gameObject.SetActive(false);
-        if (isRight)
-        {
-            rightJoystick = this;
-        }
-        else
-        {
-            leftJoystick = this;
-        }
     }
 
     public override void OnDrag(PointerEventData eventData)
     {
-        if (!isRight)
-        {
-            Vector2 direction = eventData.position - joystickCenter;
-            inputVector = (direction.magnitude > background.sizeDelta.x / 2f) ? direction.normalized : direction / (background.sizeDelta.x / 2f);
-            ClampJoystick();
-            handle.anchoredPosition = (inputVector * background.sizeDelta.x / 2f) * handleLimit;
-        }
+        Vector2 direction = eventData.position - joystickCenter;
+        inputVector = (direction.magnitude > background.sizeDelta.x / 2f) ? direction.normalized : direction / (background.sizeDelta.x / 2f);
+        ClampJoystick();
+        handle.anchoredPosition = (inputVector * background.sizeDelta.x / 2f) * handleLimit;
     }
 
     public override void OnPointerDown(PointerEventData eventData)
@@ -35,11 +24,6 @@ public class FloatingJoystick : Joystick
         background.position = eventData.position;
         handle.anchoredPosition = Vector2.zero;
         joystickCenter = eventData.position;
-
-        if (isRight) 
-        {
-
-        }
     }
 
     public override void OnPointerUp(PointerEventData eventData)
